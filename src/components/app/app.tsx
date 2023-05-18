1d670a85a989b51d50f34eb5ad8b115ad20b8894
TheSm0ke <danilsmith448@gmail.com>
2023-05-18 23:56:58 +0300

    start

---
+++ src/components/app/app.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../header/header";
+import { Content } from "../content/content";
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <Content />
+    </>
+  );
+}
---
+++ src/components/header/header.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <div className="header">
+      <div className="logo">LOGOTYPE</div>
+      <div className="menu"> menu </div>
+    </div>
+  );
+}
