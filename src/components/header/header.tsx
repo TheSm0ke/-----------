43f17e344f78316ef64c9f3c5d963916250b6dd7
TheSm0ke <danilsmith448@gmail.com>
2023-05-21 12:06:06 +0300

    Выезжающие меню

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
 import Logo from "../logo/logo";
 import Menu from "../menu/menu";
 import "./header.css";
 
 const Header = () => {
+  const [backMenu, setBackMenu] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 200) {
+      setBackMenu(true);
+    } else {
+      setBackMenu(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  if (backMenu)
+    return (
+      <div className="header" style={{ top: "-200px" }}>
+        <Logo />
+        <Menu />
+      </div>
+    );
+
   return (
     <div className="header">
       <Logo />
