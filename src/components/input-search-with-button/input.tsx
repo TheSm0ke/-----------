3de4bd82c5e471bc4e67072f81cc86bd645bc399
Saiyan <danilsmith448@gmail.com>
2023-05-22 16:56:11 +0300

    add input

--- src/components/input-search-with-button/input.tsx
+++ src/components/input-search-with-button/input.tsx
@@ -1,11 +1,20 @@
 import { search } from "data/data";
+import { useState } from "react";
 import "./input.css";
 
 const Input = () => {
+  const [valueInput, setValueInput] = useState("");
+
+  const handleKeyDown = (el) => {
+    console.log(el.target.value);
+    valueInput;
+    setValueInput(el.target.value);
+  };
+
   return (
     <div className="menu-search">
+      <input className="menu-search-input" onChange={handleKeyDown} />
       <div className="menu-search-button">{search}</div>
-      <input className="menu-search-input" />
     </div>
   );
 };
